88ebce66e6b9e10629b3df38f012c1337649e80e
Your Name <you@example.com>
2026-02-11 10:45:17 +0900

    Fix imports in UnsubscribePage

--- web/app/unsubscribe/[token]/page.tsx
+++ web/app/unsubscribe/[token]/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
 import { processUnsubscribe } from '../actions'
 
 export default function UnsubscribePage() {
